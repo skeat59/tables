406314fe580b2297c925393c31ba68d013dddf25
skeat59 <gdalia_t@epita.fr>
2013-11-19 19:51:15 +0100

    test

--- tables/Program.cs
+++ tables/Program.cs
@@ -16,7 +16,6 @@ namespace tables
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            //test git
         }
     }
 }
